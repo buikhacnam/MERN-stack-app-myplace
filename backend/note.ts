6d534fda50769cdd18c0d86f68f73c879cfcd694
Bui Nam <nambk@bravebits.vn>
2021-06-03 23:05:55 +0700

    update file note

--- backend/note.ts
+++ backend/note.ts
@@ -23,3 +23,51 @@ let place = {
     image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fhatrabbits.com%2Fen%2Frandom-image%2F&psig=AOvVaw3j8C0Ig12AmUNZ0gMc78ZL&ust=1622599978362000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCc1Iyu9fACFQAAAAAdAAAAABAD',
     __v: 0
 }
+
+//user populated place: 
+{
+    "places": [
+        {
+            "location": {
+                "lat": 40.7484474,
+                "lng": -73.9871516
+            },
+            "_id": "60b8ef9fca2e1829c4aae2d7",
+            "title": "yyyyyyyyyyyyyyyyyyyyyyyyyy",
+            "description": "this description has changed now",
+            "address": "da nang city",
+            "creator": "60b8e497c8f4bb32942b8408",
+            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhatrabbits.com%2Fen%2Frandom-image%2F&psig=AOvVaw3j8C0Ig12AmUNZ0gMc78ZL&ust=1622599978362000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCc1Iyu9fACFQAAAAAdAAAAABAD",
+            "__v": 0,
+            "id": "60b8ef9fca2e1829c4aae2d7"
+        },
+        {
+            "location": {
+                "lat": 40.7484474,
+                "lng": -73.9871516
+            },
+            "_id": "60b8f5dffe3e40341c7b85c8",
+            "title": "aaaaaaaaaaaaaaaaaaaaaaaa",
+            "description": "this description has changed now",
+            "address": "da nang city",
+            "creator": "60b8e497c8f4bb32942b8408",
+            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhatrabbits.com%2Fen%2Frandom-image%2F&psig=AOvVaw3j8C0Ig12AmUNZ0gMc78ZL&ust=1622599978362000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCc1Iyu9fACFQAAAAAdAAAAABAD",
+            "__v": 0,
+            "id": "60b8f5dffe3e40341c7b85c8"
+        },
+        {
+            "location": {
+                "lat": 40.7484474,
+                "lng": -73.9871516
+            },
+            "_id": "60b8f5effe3e40341c7b85c9",
+            "title": "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            "description": "this description has changed now",
+            "address": "da nang city",
+            "creator": "60b8e497c8f4bb32942b8408",
+            "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhatrabbits.com%2Fen%2Frandom-image%2F&psig=AOvVaw3j8C0Ig12AmUNZ0gMc78ZL&ust=1622599978362000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCc1Iyu9fACFQAAAAAdAAAAABAD",
+            "__v": 0,
+            "id": "60b8f5effe3e40341c7b85c9"
+        }
+    ]
+}
