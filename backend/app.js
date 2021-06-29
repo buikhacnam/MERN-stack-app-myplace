500859575b59479a19d427cef50c46e62b9ff716
Bui Nam <nambk@bravebits.vn>
2021-06-29 21:24:35 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
 
 mongoose
 	.connect(
-		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jntsz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+		process.env.MONGO_URI,
 		{
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
